6cf9936251c3d33f1056fd7f18f643e389f31c76
suhJh <sjs0701@me.com>
2016-09-27 22:56:13 +0900

    so sleepy..
    changed a lot.
    
    im not sure if i use bower or not

---
+++ config/gulp-config.js
@@ -0,0 +1,20 @@
+const src = 'src';
+const dist = 'public';
+
+
+
+module.exports = {
+    src : {
+        js : src + '/js',
+        css : src + '/css',
+        bower : dist + '/bower',
+        html : dist
+    },
+    dist :{
+        bundle : dist + '/js/build',
+        js : dist + '/js',
+        css : dist + '/css,',
+        bower : dist + '/bower',
+        html : dist
+    }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,46 @@ const source = require('vinyl-source-stream');
 const browserSync = require('browser-sync');
 
 const config = require('./config/gulp-config');
+
+
+
+gulp.task('server', function () {
+
+  /* 여기에 express 키는 소스와야 함*/
+
+	browserSync({
+		socket: {
+			domain: "localhost:3000"
+		},
+		proxy: 'localhost:8080',
+		open: 'external'
+	});
+
+	return()=>{console.log('hey! browserSync started');};
+});
+
+
+gulp.task('build', function () {
+
+  /* deep copy하는 모듈설정 와야함*/
+  /* build 실패시 서버안끊기도록 하는 소스 와야함*/
+
+	return browserify(config.src.js + '/main.js')
+		.transform(babelify, {presets: ['es2015']})
+		.bundle()
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.dist.bundle + '/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+
+gulp.task('watch', function () {
+	gulp.watch( config.src.js + '/**/*.*' , ['build']).on('change', browserSync.reload);
+});
+
+
+gulp.task('default', [
+	'server',
+	'build',
+	'watch'
+	]);
